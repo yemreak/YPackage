219ff94a696f50cc7a4ad49b8c84a0a07f9baeaf
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-11-30 19:39:17 +0300

    🚀 Artık pattern ` \* ` desteklemekte

--- ypackage/integrate.py
+++ ypackage/integrate.py
@@ -120,111 +120,123 @@ def updateSubSummaries(config: dict, startpath: str, index: str = "Index", push=
         github.push_to_github(startpath, paths, COMMIT_UPDATE_SUBMODULES)
 
 
-def generate_changelog():
-    pass
+def parse_args():
+    global parser
+    if 'parser' in globals():
+        return parser.parse_args()
+    else:
+        parser = argparse.ArgumentParser(
+            description='Create `README.md` and `SUMMARY.md` file for GitBook synchronization',
+        )
+        parser.add_argument(
+            'paths',
+            nargs="+",
+            metavar='paths',
+            help='Projelerin yolları',
+        )
+        parser.add_argument(
+            "--update",
+            "-u",
+            action="store_true",
+            dest="update",
+            help="Alt modüllerin summary'lerini güncelleme"
+        )
+        parser.add_argument(
+            "--recreate",
+            "-r",
+            action="store_true",
+            dest="recreate",
+            help="Summary dosyasını baştan oluşturur"
+        )
+        parser.add_argument(
+            "--generate",
+            "-g",
+            action="store_true",
+            dest="generate",
+            help="Markdown dışı dosyalar için README'ye bağlantılar oluşturma"
+        )
+        parser.add_argument(
+            '--level-limit',
+            '-ll',
+            dest="level_limit",
+            default=-1,
+            help='Fonksiyonların çalışacağı en yüksek derinlik (0 => ana dizinde çalışır sadece)',
+            type=int,
+        )
+        parser.add_argument(
+            "--debug",
+            "-d",
+            action="store_true",
+            dest="debug",
+            help="Bilgilendirici metinleri gösterme"
+        )
+        parser.add_argument(
+            "--store",
+            "-s",
+            action="store_true",
+            dest="store",
+            help="Son komutu hızlı kullanım için yapılandırma dosyasında saklar"
+        )
+        parser.add_argument(
+            "--push",
+            "-p",
+            action="store_true",
+            dest="push",
+            help="İşlem sonrası GitHub'a otomatik pushlar"
+        )
+        # WARN: Kullanışsız parametreler
+        parser.add_argument(
+            '--index',
+            '-ix',
+            dest="indexStr",
+            default="Index",
+            help='Generated string will be inserted between given indexes',
+            type=str,
+        )
+        # BUG: Bu yapı çalışmaz, nargs olması lazım
+        parser.add_argument(
+            '--privates',
+            "-pp",
+            dest="privates",
+            default=["res", "__pycache__"],
+            help='List of folder names that you dont want to add',
+            type=list
+        )
+        parser.add_argument(
+            "--footer-path",
+            '-fp',
+            dest="footerPath",
+            help="Append the given file to end of the output. "
+        )
+        parser.add_argument(
+            '--new-index',
+            '-nix',
+            dest="newIndex",
+            default=None,
+            help='Generated string will be inserted between given indexes with changing the indexes',
+            type=str,
+        )
+
+        return parser.parse_args()
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='Create `README.md` and `SUMMARY.md` file for GitBook synchronization',
-    )
-    parser.add_argument(
-        'paths',
-        nargs="+",
-        metavar='paths',
-        help='Projelerin yolları',
-    )
-    parser.add_argument(
-        "--update",
-        "-u",
-        action="store_true",
-        dest="update",
-        help="Alt modüllerin summary'lerini güncelleme"
-    )
-    parser.add_argument(
-        "--recreate",
-        "-r",
-        action="store_true",
-        dest="recreate",
-        help="Summary dosyasını baştan oluşturur"
-    )
-    parser.add_argument(
-        "--generate",
-        "-g",
-        action="store_true",
-        dest="generate",
-        help="Markdown dışı dosyalar için README'ye bağlantılar oluşturma"
-    )
-    parser.add_argument(
-        '--level-limit',
-        '-ll',
-        dest="level_limit",
-        default=-1,
-        help='Fonksiyonların çalışacağı en yüksek derinlik (0 => ana dizinde çalışır sadece)',
-        type=int,
-    )
-    parser.add_argument(
-        "--debug",
-        "-d",
-        action="store_true",
-        dest="debug",
-        help="Bilgilendirici metinleri gösterme"
-    )
-    parser.add_argument(
-        "--store",
-        "-s",
-        action="store_true",
-        dest="store",
-        help="Son komutu hızlı kullanım için yapılandırma dosyasında saklar"
-    )
-    parser.add_argument(
-        "--push",
-        "-p",
-        action="store_true",
-        dest="push",
-        help="İşlem sonrası GitHub'a otomatik pushlar"
-    )
-    # WARN: Kullanışsız parametreler
-    parser.add_argument(
-        '--index',
-        '-ix',
-        dest="indexStr",
-        default="Index",
-        help='Generated string will be inserted between given indexes',
-        type=str,
-    )
-    # BUG: Bu yapı çalışmaz, nargs olması lazım
-    parser.add_argument(
-        '--privates',
-        "-pp",
-        dest="privates",
-        default=["res", "__pycache__"],
-        help='List of folder names that you dont want to add',
-        type=list
-    )
-    parser.add_argument(
-        "--footer-path",
-        '-fp',
-        dest="footerPath",
-        help="Append the given file to end of the output. "
-    )
-    parser.add_argument(
-        '--new-index',
-        '-nix',
-        dest="newIndex",
-        default=None,
-        help='Generated string will be inserted between given indexes with changing the indexes',
-        type=str,
-    )
-
-    args = parser.parse_args()
-    PATHS = args.paths
-
-    new_args_start_index = (len(PATHS) + 1)
+def integrate(paths, safe=False):
+    new_args_start_index = (len(paths) + 1)
     override = len(sys.argv) > new_args_start_index
 
-    for path in PATHS:
-        if os.path.isdir(path):
+    for path in paths:
+        if not safe and path[-2:] == "/*":
+            dirname = path[:-2]
+
+            new_paths = []
+            for path in os.listdir(dirname):
+                new_path = os.path.join(dirname, path)
+                if os.path.isdir(new_path):
+                    new_paths.append(new_path)
+
+            integrate(new_paths, safe=True)
+
+        elif os.path.isdir(path):
             config = read_config(path, INTEGRATION_FILE)
 
             if not override:
@@ -236,14 +248,15 @@ def main():
 
                 if new_args:
                     sys.argv = [__file__, path] + new_args.split()
-                    args = parser.parse_args()
+                    new_args_start_index = 2  # len(paths) + 1
                 else:
                     print(f"{os.path.basename(path)} için entegrasyon özelliği mevcut değil")
                     print(
                         f"    `{INTEGRATION_FILE}` dosyası içerisindeki `integration` alanında `args` özelliği yok")
                     continue
 
-            PRIVATES, INDEX_STR, NEW_INDEX_STR, FOOTER_PATH, LEVEL_LIMIT, DEBUG, UPDATE, RECREATE, GENERATE, STORE, PUSH = args.privates, args.indexStr, args.newIndex, args.footerPath, args.level_limit, args.debug, args.update, args.recreate, args.generate, args.store, args.push
+            args = parse_args()
+            PRIVATES, INDEX_STR, NEW_INDEX_STR, FOOTER_PATH, LEVEL_LIMIT, UPDATE, RECREATE, GENERATE, STORE, PUSH = args.privates, args.indexStr, args.newIndex, args.footerPath, args.level_limit, args.update, args.recreate, args.generate, args.store, args.push
 
             if STORE:
                 last_args = sys.argv[new_args_start_index:]
@@ -282,8 +295,17 @@ def main():
             if UPDATE:
                 updateSubSummaries(config, path, INDEX_STR, push=PUSH)
 
-        elif DEBUG:
+        else:
             print(f"Hatalı yol: {path}")
+
+
+def generate_changelog():
+    pass
+
+
+def main():
+    args = parse_args()
+    integrate(args.paths)
 
 
 if __name__ == "__main__":
