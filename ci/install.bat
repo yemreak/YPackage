ead8193576ac59970aefdcf98bb4d3c1cd9a4a2b
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-02-23 19:31:45 +0300

    👨‍🔧

--- ci/install.bat
+++ ci/install.bat
@@ -1,6 +1,6 @@
 python -m venv venv
 call venv\Scripts\activate.bat
-python -m pip install --upgrade pip setuptools wheel
+python -m pip install --upgrade pip 
 python -m pip install --upgrade -r .\ci\requirements.txt
 python -m pip install --upgrade .
 call deactivate
