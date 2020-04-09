de7f0503063d6a91309b005c951a889658dd86c3
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2020-04-10 00:22:42 +0300

    🧪 Ufak test işlemleri eklendi

--- src/test/model/input.py
+++ src/test/model/input.py
@@ -1,4 +1,4 @@
-
+import time
 from ...ypackage.model.input import InputRecorder
 
 
@@ -6,4 +6,10 @@ class TestInput:
 
     def test_input_recorder(self):
         inputRecorder = InputRecorder()
+
         assert inputRecorder.record()
+
+        time.sleep(2)
+        assert inputRecorder.stop()
+
+        assert inputRecorder.events
